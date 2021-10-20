d75452855cdcd7efe83fa6519e3d3154f3d07d34
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-10-20 00:48:46 -0300

    exercises 7,8,9 and 10

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days) 
+print("Here are the months:", months)
+
+print(""" 
+Aqui acontece algo estranho.
+Com tres aspas duplas.
+Nos conseguimos escrever como queriamos.
+Pode ser com 4 linhas ou 5, ou 6.
+""")
